3f31efaa7f9b19c68f7c623354e8d32c80408814
lime999 <104721492+lime999@users.noreply.github.com>
2025-05-13 18:27:32 +0200

    prototype loop until won or lost

--- mastermind.ts
+++ mastermind.ts
@@ -8,8 +8,14 @@ let numberOfCorrect: number = 0
 let numberOfFalse: number = 0
 let numberOfInCombination: number = 0
 let solvedOrLost: boolean = false
+let numberOfTries: number = 0
+let loopRunning = false;
 let colors: Array<string> = ["blue", "white", "red", "black", "orange", "green", "yellow", "grey"]
 console.log("The allowed colors are: " + colors)
+if (numberOfTries > 11) {
+  solvedOrLost = true
+}
+
 
 // create solution
 
@@ -21,63 +27,78 @@ for (let i = 0; i < 4; i++) {
 }
 console.log(solution)
 
+
 // while not solved:
+loopRunning = true
+if (loopRunning == true && solvedOrLost ==  false) {
+  loop()
+  loopRunning = false
+}
+function loop() {
+  loopRunning = false;
 
-// get input(guess) from user
-rl.question('Guess the solution, e.g. "red,white,red,orange":\n', (answer) => {
-  currentGuess = answer.split(",")
+  // get input(guess) from user
 
-  // stop if invalid answer
+  rl.question('Guess the solution, e.g. "red,white,red,orange":\n', (answer) => {
+    currentGuess = answer.split(",")
 
-  if (currentGuess.every(color => colors.includes(color)) !== true) {
-    console.log("Invalid Answer!")
-    rl.close()
-    return
-  }
-  console.log(currentGuess)
+    // stop if invalid answer
 
-  // compare guess and solution
-
-  for (let i = 0; i !== 4; i++) {
-
-    // checking for completely correct colors
-
-    if (currentGuess[i] == solution[i]) {
-      numberOfCorrect += 1
-      completelyCorrect.push(currentGuess[i])
+    if (currentGuess.every(color => colors.includes(color)) !== true) {
+      console.log("Invalid Answer!")
+      rl.close()
+      return
     }
+    console.log(currentGuess)
 
-    // checking for false colors
+    // compare guess and solution
 
-    else if (solution.includes(currentGuess[i]) !== true) {
-      numberOfFalse += 1
-    }
-  }
+    for (let i = 0; i !== 4; i++) {
 
-  // checking for colors in the wrong position
+      // checking for completely correct colors
 
-  for (let i = 0; i !== 4; i++) {
-    let hasMatched = false
-    for (let a = 0; a !== 4; a++) {
-      if (currentGuess[i] == solution[a] && completelyCorrect[i] !== solution[a] && hasMatched == false) {
-        numberOfInCombination += 1
-        hasMatched = true
+      if (currentGuess[i] == solution[i]) {
+        numberOfCorrect += 1
+        completelyCorrect.push(currentGuess[i])
+      }
+
+      // checking for false colors
+
+      else if (solution.includes(currentGuess[i]) !== true) {
+        numberOfFalse += 1
       }
     }
-    hasMatched = false
-  }
 
-  // give feedback
+    // checking for colors in the wrong position
 
-  if (numberOfCorrect < 4) {
-    console.log("Your Guess had " + (numberOfCorrect < 1 ? "no" : numberOfCorrect) + " completely correct color(s), " + (numberOfFalse < 1 ? "no" : numberOfFalse) + " false color(s), and " + (numberOfInCombination < 1 ? "no" : numberOfInCombination) + " color(s) that were correct but in the wrong place")
-  } else if (numberOfCorrect == 4) {
-    console.log("Congratulations,\nyou have won the game")
-    solvedOrLost = true
-  }
-  rl.close();
-})
+    for (let i = 0; i !== 4; i++) {
+      let hasMatched = false
+      for (let a = 0; a !== 4; a++) {
+        if (currentGuess[i] == solution[a] && completelyCorrect[i] !== solution[a] && hasMatched == false) {
+          numberOfInCombination += 1
+          hasMatched = true
+        }
+      }
+      hasMatched = false
+    }
+    // give feedback
+    // if solved stop 
+    if (numberOfCorrect < 4) {
+      console.log("Your Guess had " + (numberOfCorrect < 1 ? "no" : numberOfCorrect) + " completely correct color(s), " + (numberOfFalse < 1 ? "no" : numberOfFalse) + " false color(s), and " + (numberOfInCombination < 1 ? "no" : numberOfInCombination) + " color(s) that were correct but in the wrong place")
+      numberOfTries + 1
+      loopRunning = true
+    } else if (numberOfCorrect == 4) {
+      console.log("Congratulations,\nyou have won the game!")
+      solvedOrLost = true
+      rl.close()
+    }
+  })
+
+
+}
 
 
 
-// if solved stop 
+
+
+
