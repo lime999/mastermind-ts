74b5bfb1bea47775f04ac0942f60cda89f620949
lime999 <104721492+lime999@users.noreply.github.com>
2025-06-24 20:16:42 +0200

    making getInputAndCompare function in a seperate file

--- src/getInputAndCompare.ts
+++ src/getInputAndCompare.ts
@@ -1,13 +1,19 @@
-import { checkSolution } from './checkSolution';
 import * as readline from 'readline'
 import { pinColors } from './pin';
-import { giveFeedback } from './giveFeedback';
+import { createSolution } from './create-solution';
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
+let numberOfCorrect: number = 0
+let numberOfFalse: number = 0
+let numberOfInCombination: number = 0
+let solvedOrLost: boolean = false
+let numberOfTries: number = 0
+let completelyCorrect: Array<string> = []
+let solution = createSolution()
 export function getInputAndCompare() {
+
     rl.question('Guess the solution, e.g. "red,white,red,orange":\n', (answer) => {
         var currentGuess: Array<string> = answer.split(",")
 
@@ -22,9 +28,45 @@ export function getInputAndCompare() {
 
         // compare guess and solution
 
-        checkSolution();
+
+        for (let i = 0; i !== 4; i++) {
+
+            // checking for completely correct colors
+            if (currentGuess[i] == solution[i]) {
+                numberOfCorrect += 1;
+                completelyCorrect.push(currentGuess[i]);
+            }
 
 
-        giveFeedback()
+
+            // checking for false colors
+            else if (solution.includes(currentGuess[i]) !== true) {
+                numberOfFalse += 1;
+            }
+        }
+
+        // checking for colors in the wrong position
+
+        for (let i = 0; i !== 4; i++) {
+            let hasMatched = false
+            for (let a = 0; a !== 4; a++) {
+                if (currentGuess[i] == solution[a] && completelyCorrect[i] !== solution[a] && hasMatched == false) {
+                    numberOfInCombination += 1
+                    hasMatched = true
+                }
+            }
+            hasMatched = false
+
+        }
+
+        // give feedback
+    if (numberOfCorrect < 4) {
+        console.log("Your Guess had " + (numberOfCorrect < 1 ? "no" : numberOfCorrect) + " completely correct color(s), " + (numberOfFalse < 1 ? "no" : numberOfFalse) + " false color(s), and " + (numberOfInCombination < 1 ? "no" : numberOfInCombination) + " color(s) that were correct but in the wrong place")
+        numberOfTries++
+    } else if (numberOfCorrect == 4) {
+        console.log("Congratulations,\nyou have won the game!")
+        solvedOrLost = true
+    }
+
     })
 }
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,21 @@
 import * as readline from 'readline'
 import { createSolution } from './create-solution';
 import { pinColors } from './pin';
+import { getInputAndCompare } from './getInputAndCompare';
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-let numberOfCorrect: number = 0
-let numberOfFalse: number = 0
-let numberOfInCombination: number = 0
+let solution = createSolution()
 let solvedOrLost: boolean = false
 let numberOfTries: number = 0
-let loopRunning = false;
-let completelyCorrect: Array<string> = []
 console.log("The allowed colors are: " + pinColors)
 if (numberOfTries > 11) {
   solvedOrLost = true
 }
 
-let solution = createSolution()
+getInputAndCompare()
+
 
 // while not solved:
 
